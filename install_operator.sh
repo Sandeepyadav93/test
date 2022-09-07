7da2878e5216ae0200f5a675ae5a67ee5043686c
Sandeep Yadav <sandyada@redhat.com>
2022-09-07 07:46:41 +0530

    Update install_operator.sh

--- install_operator.sh
+++ install_operator.sh
@@ -2,17 +2,17 @@
 eval $(crc oc-env)
 oc login -u kubeadmin -p 12345678 https://api.crc.testing:6443
 cd $HOME/install_yamls
-echo "make crc_storage" &>> $HOME/install_operator.log
+echo "make crc_storage"
 make crc_storage
-echo "make mariadb" &>> $HOME/install_operator.log
+echo "make mariadb"
 make mariadb
-sleep 30 
-echo "make keystone" &>> $HOME/install_operator.log
+sleep 30
+echo "make keystone"
 make keystone
 sleep 30
-echo "make mariadb_deploy" &>> $HOME/install_operator.log
+echo "make mariadb_deploy"
 make mariadb_deploy
 sleep 60
-echo "make keystone_deploy" &>> $HOME/install_operator.log
+echo "make keystone_deploy"
 make keystone_deploy
 sleep 90
