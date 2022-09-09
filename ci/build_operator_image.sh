5b9dd1114f8f47f859a8f72234b75a9df84f1347
Sandeep Yadav <sandyada@redhat.com>
2022-09-09 16:51:40 +0530

    Update build_operator_image.sh

--- ci/build_operator_image.sh
+++ ci/build_operator_image.sh
@@ -4,6 +4,7 @@ eval $(crc oc-env)
 oc login -u kubeadmin -p 12345678 https://api.crc.testing:6443
 cd $HOME/keystone-operator
 OS_REGISTRY=$(oc get route default-route -n openshift-image-registry --template='{{ .spec.host }}')
+oc registry login --skip-check ${OS_REGISTRY}
 #workaround to avoid "Error: error creating build container: short-name resolution enforced but cannot prompt without a TTY"
 podman pull docker.io/library/golang:1.18
 IMAGE_TAG_BASE=${OS_REGISTRY}/$(oc project -q)/keystone-operator VERSION=0.0.1 IMG=$IMAGE_TAG_BASE:v$VERSION make manifests build docker-build docker-push bundle bundle-build bundle-push catalog-build catalog-push
