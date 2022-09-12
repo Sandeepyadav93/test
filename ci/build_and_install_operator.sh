70fd702c2177e7fe150b03c7b6488d1cacf22efe
Sandeep Yadav <sandyada@redhat.com>
2022-09-12 20:25:26 +0530

    build and install in same command

---
+++ ci/build_and_install_operator.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -ex
+eval $(crc oc-env)
+oc login -u kubeadmin -p 12345678 https://api.crc.testing:6443
+cd $HOME/install_yamls/
+make namespace
+cd $HOME
+git clone https://github.com/openstack-k8s-operators/$1.git 
+cd $HOME/$1
+OS_REGISTRY=$(oc get route default-route -n openshift-image-registry --template='{{ .spec.host }}')
+oc registry login --skip-check ${OS_REGISTRY}
+#workaround to avoid "Error: error creating build container: short-name resolution enforced but cannot prompt without a TTY"
+podman pull docker.io/library/golang:1.18
+IMAGE_TAG_BASE=${OS_REGISTRY}/$(oc project -q)/$1 VERSION=0.0.1 IMG=$IMAGE_TAG_BASE:v$VERSION make manifests build docker-build docker-push bundle bundle-build bundle-push catalog-build catalog-push
+oc get is
+oc set image-lookup --all
+
+export $(echo ${1^^} | cut -d '-' -f1)_IMG=default-route-openshift-image-registry.apps-crc.testing/openstack/$1-index:v0.0.1
+
+echo $(echo ${1^^} | cut -d '-' -f1)_IMG
+
+cd $HOME/install_yamls
+make mariadb
+sleep 30
+make keystone
+sleep 30
+make mariadb_deploy
+sleep 60
+make keystone_deploy
+sleep 90
