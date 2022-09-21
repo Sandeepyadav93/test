b446001c5bdeb84a8020d376bac204dcab0be7fa
Sandeep Yadav <sandyada@redhat.com>
2022-09-21 14:40:47 +0530

    Update install_operator_with_hash.sh

--- .github/install_operator_with_hash.sh
+++ .github/install_operator_with_hash.sh
@@ -3,7 +3,7 @@ set -ex
 export $(echo ${1^^} | cut -d '-' -f1)_IMG=$2/$1-index:$3
 cd $HOME
 rm -rf install_yamls
-git clone https://github.com/openstack-k8s-operators/install_yamls.git
+git clone https://github.com/Sandeepyadav93/install_yamls.git
 cd $HOME/install_yamls
 make crc_storage
 sleep 20
@@ -11,7 +11,7 @@ make mariadb
 sleep 90
 make keystone
 sleep 90
-make mariadb_deploy SHELL=/bin/bash
+make mariadb_deploy
 sleep 90
-make keystone_deploy SHELL=/bin/bash
+make keystone_deploy
 sleep 90
