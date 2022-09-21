0cfa9bed6bdc131ddf3910c521512b62779c7477
Sandeep Yadav <sandyada@redhat.com>
2022-09-21 07:06:09 +0530

    Update install_operator_with_hash.sh

--- .github/install_operator_with_hash.sh
+++ .github/install_operator_with_hash.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -ex
-eval $(crc oc-env)
-oc login -u kubeadmin -p 12345678 https://api.crc.testing:6443
 export $(echo ${1^^} | cut -d '-' -f1)_IMG=$2/$1-index:$3
-
 cd $HOME/install_yamls
 make mariadb
 sleep 90
