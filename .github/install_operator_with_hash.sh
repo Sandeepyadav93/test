da5c87ccc648fcc8258b64f57ece8ba0f2bb22bb
Sandeep Yadav <sandyada@redhat.com>
2022-09-21 07:05:32 +0530

    Create install_operator_with_hash.sh

---
+++ .github/install_operator_with_hash.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -ex
+eval $(crc oc-env)
+oc login -u kubeadmin -p 12345678 https://api.crc.testing:6443
+export $(echo ${1^^} | cut -d '-' -f1)_IMG=$2/$1-index:$3
+
+cd $HOME/install_yamls
+make mariadb
+sleep 90
+make keystone
+sleep 90
+make mariadb_deploy
+sleep 90
+make keystone_deploy
+sleep 90
